e4e37cc18e185cfc532b18eba110b7f84ddbb5a9
Agus12344 <48695094+AgustinIbanez00@users.noreply.github.com>
2022-02-11 01:20:50 -0300

    Appsettings.json

--- BlazorPWA/Program.cs
+++ BlazorPWA/Program.cs
@@ -10,7 +10,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration.GetValue<string>("HostUrl")) });
 builder.Services.AddScoped<IDistribucionService, DistribucionService>();
 builder.Services.AddScoped<IDestinoService, DestinoService>();
 builder.Services.AddScoped<IViajeService, ViajeService>();
